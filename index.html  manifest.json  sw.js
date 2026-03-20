5720424335a2f0524d98ca331ca395339ea18d0b
JMAR1976 <jmar.rodrigues@gmail.com>
2026-03-20 23:45:11 +0000

    Update index.html  manifest.json  sw.js

--- index.html  manifest.json  sw.js
+++ index.html  manifest.json  sw.js
@@ -1,4 +1,15 @@
-const API_KEY = 2d89f5a76883471f94d8f753c5794413
-index.html
-manifest.json
-sw.js
+
+async function loadNews() {
+  const res = await fetch("https://api.spaceflightnewsapi.net/v4/articles/");
+  const data = await res.json();
+  articles = data.results;
+
+  articles = articles.map(a => ({
+    title: a.title,
+    description: a.summary,
+    url: a.url
+  }));
+
+  renderNews(articles);
+  showTrends();
+}
